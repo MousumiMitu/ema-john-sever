7990826fbe08f7dc73feed09cc697e3e388b8f14
MousumiMitu <mousumitu@gmail.com>
2021-03-29 00:29:01 +0600

    root api created

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send("hello ! db working");
+});
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
