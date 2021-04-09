40bea568a3f60c20d1acfbdf0ca6309e1191ad0c
MousumiMitu <mousumitu@gmail.com>
2021-04-09 19:30:58 +0600

    pass up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1el86.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:159mitu357@cluster0.1el86.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
